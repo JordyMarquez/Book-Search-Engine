f063bdcc1e2c9db81866c5a51c00d84305ec8016
Jordy Marquez <jordymarquez@MacBook-Pro.local>
2023-05-11 09:24:16 -0700

    fixed typo in app.js

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -3,8 +3,8 @@
 
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import {useMutation} from '@apollo/client'
-import {ADD_USER } from '../utils/mutations' 
+import { useMutation} from '@apollo/client';
+import { ADD_USER } from '../utils/mutations';
 import Auth from '../utils/auth';
 
 // import { createUser } from '../utils/API';
